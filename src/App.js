339de222a46e6eb196ca4d9da05b718f0b6274db
erol23 <hurkanrol15@gmail.com>
2022-12-16 15:40:12 +0100

    added component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem"
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <p>This is also visible!</p>
+      <ExpenseItem/>
     </div>
   );
 }
